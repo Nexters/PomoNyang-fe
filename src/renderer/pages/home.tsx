57d403c169e5c51781d7887319030071fc95684a
halang <lhr4884@naver.com>
2024-07-14 21:33:46 +0900

    feat: 초기 세팅 & prettier & eslint (#2)

---
+++ src/renderer/pages/home.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>home</h1>
+      <Button>클릭</Button>
+    </div>
+  );
+};
+
+export default Home;
---
+++ vite.renderer.config.ts
@@ -0,0 +1,28 @@
+import path from 'path';
+import type { ConfigEnv, UserConfig } from 'vite';
+import { defineConfig } from 'vite';
+import { pluginExposeRenderer } from './vite.base.config';
+
+// https://vitejs.dev/config
+export default defineConfig((env) => {
+  const forgeEnv = env as ConfigEnv<'renderer'>;
+  const { root, mode, forgeConfigSelf } = forgeEnv;
+  const name = forgeConfigSelf.name ?? '';
+
+  return {
+    root,
+    mode,
+    base: './',
+    build: {
+      outDir: `.vite/renderer/${name}`,
+    },
+    plugins: [pluginExposeRenderer(name)],
+    resolve: {
+      preserveSymlinks: true,
+      alias: {
+        '@': path.resolve(__dirname, './src/renderer'),
+      },
+    },
+    clearScreen: false,
+  } as UserConfig;
+});
