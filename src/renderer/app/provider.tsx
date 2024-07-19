8743f502345860ebcaca37fb3b09457984142a9c
leeharyung <lhr4884@naver.com>
2024-07-20 01:58:49 +0900

    프로바이더 수정

--- src/renderer/app/provider.tsx
+++ src/renderer/app/provider.tsx
@@ -1,21 +1,41 @@
 import { PropsWithChildren } from 'react';
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { createSyncStoragePersister } from '@tanstack/query-sync-storage-persister';
+import { QueryClient } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client';
+
+import { DEFAULT_KEY, updateTimer } from '@/entities/timer';
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       retry: 0,
+      staleTime: 1000 * 60 * 60, // 60분
     },
   },
 });
 
+// 오프라인 상황에서 mutation을 저장하고, 온라인 상황에서 다시 실행
+queryClient.setMutationDefaults([DEFAULT_KEY], {
+  mutationFn: updateTimer,
+});
+
+const persister = createSyncStoragePersister({
+  storage: window.localStorage,
+});
+
 export const Provider = ({ children }: PropsWithChildren) => {
   return (
-    <QueryClientProvider client={queryClient}>
+    <PersistQueryClientProvider
+      client={queryClient}
+      persistOptions={{ persister }}
+      onSuccess={async () => {
+        await queryClient.resumePausedMutations();
+      }}
+    >
       {children}
       <ReactQueryDevtools buttonPosition="bottom-left" initialIsOpen={false} />
-    </QueryClientProvider>
+    </PersistQueryClientProvider>
   );
 };
