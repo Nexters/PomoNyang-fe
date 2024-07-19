1aff36c3fef7ce95bac67c8b47897a0e6201fa68
young-do <yd199323@gmail.com>
2024-07-19 23:39:02 +0900

    클릭 이벤트 추가하여 캔버스 내 버튼과 상호작용 기능 추가

--- src/renderer/pages/selection.tsx
+++ src/renderer/pages/selection.tsx
@@ -15,7 +15,7 @@ const useRiveStateMachineInput = (...args: Parameters<typeof useStateMachineInpu
   const input = useStateMachineInput(rive, stateMachineName, inputName, value);
 
   const setValue = (newValue: number) => {
-    if (input) {
+    if (input && value !== newValue) {
       input.value = newValue;
       _setValue(newValue);
     }
@@ -35,9 +35,33 @@ const Selection = () => {
   });
   const [value, setValue] = useRiveStateMachineInput(rive, STATE_MACHINE_NAME, INPUT_NAME, 0);
 
+  const handleClick = (e: React.MouseEvent<HTMLCanvasElement>) => {
+    // 1. 정사각형 canvas에서 클릭된 포인터 위치를 비율로 가져옴
+    // 2. 특정 x, y 범위 값이면 로직 실행
+    const { offsetX, offsetY } = e.nativeEvent;
+    const { clientWidth, clientHeight } = e.currentTarget;
+    const x = offsetX / clientWidth;
+    const y = offsetY / clientHeight;
+    console.log(e, x, y);
+    if (y > 0.78 && y < 0.82) {
+      if (x > 0.28 && x < 0.42) {
+        // click level 0
+        return setValue(0);
+      }
+      if (x > 0.42 && x < 0.56) {
+        // click level 1
+        return setValue(1);
+      }
+      if (x > 0.56 && x < 0.7) {
+        // click level 2
+        return setValue(2);
+      }
+    }
+  };
+
   return (
     <div className="flex flex-col max-w-md min-h-screen mx-auto">
-      <RiveComponent className="w-full aspect-square" />
+      <RiveComponent className="w-full aspect-square" onClick={handleClick} />
       <div className="flex gap-2">
         Choose a level:
         {new Array(3).fill(null).map((_, index) => (
