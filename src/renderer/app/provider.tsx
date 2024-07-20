37f9fbc0bb913d5f94612398396c7dc4c97f7809
leeharyung <lhr4884@naver.com>
2024-07-20 23:44:29 +0900

    keepLatestMutations 주석처리

--- src/renderer/app/provider.tsx
+++ src/renderer/app/provider.tsx
@@ -3,7 +3,11 @@ import { PropsWithChildren } from 'react';
 import { createSyncStoragePersister } from '@tanstack/query-sync-storage-persister';
 import { QueryClient } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client';
+import {
+  // PersistedClient,
+  PersistQueryClientProvider,
+  // PersistRetryer,
+} from '@tanstack/react-query-persist-client';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -14,8 +18,43 @@ const queryClient = new QueryClient({
   },
 });
 
+// 가장 최신의 mutation만 유지하는 함수
+// const keepLatestMutations: PersistRetryer = ({ persistedClient }) => {
+//   const mutations = [...persistedClient.clientState.mutations];
+//   const queries = [...persistedClient.clientState.queries];
+
+//   // mutationKey를 기준으로 가장 최신의 mutation만 유지
+//   const latestMutations = mutations.reduce(
+//     (acc, mutation) => {
+//       const key = JSON.stringify(mutation.mutationKey);
+//       acc[key] = mutation;
+//       return acc;
+//     },
+//     {} as Record<string, (typeof mutations)[0]>,
+//   );
+
+//   const client: PersistedClient = {
+//     ...persistedClient,
+//     clientState: {
+//       mutations: Object.values(latestMutations),
+//       queries,
+//     },
+//   };
+//   return client;
+// };
+
 const persister = createSyncStoragePersister({
   storage: window.localStorage,
+  // serialize: (client) => {
+  //   const optimizedClient = keepLatestMutations({
+  //     persistedClient: client as PersistedClient,
+  //     error: new Error(),
+  //     errorCount: 0,
+  //   });
+
+  //   console.log('serialize: ', optimizedClient || client);
+  //   return JSON.stringify(optimizedClient || client);
+  // },
 });
 
 export const Provider = ({ children }: PropsWithChildren) => {
