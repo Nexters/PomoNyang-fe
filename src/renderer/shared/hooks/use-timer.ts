c3ab2c440bc3127170f7e75e14bb3bd546793da2
leeharyung <lhr4884@naver.com>
2024-07-25 00:44:57 +0900

    타이머 로직 정교화

--- src/renderer/shared/hooks/use-timer.ts
+++ src/renderer/shared/hooks/use-timer.ts
@@ -7,6 +7,8 @@ type THandler = {
   onFinish?: () => void;
 };
 
+const INTERVAL_MS = 100;
+
 /**
  * @returns time: ms 단위
  */
@@ -14,8 +16,11 @@ export const useTimer = (initialTime: number, handler: THandler) => {
   const { onStart, onStop, onPause, onFinish } = handler;
 
   const [time, setTime] = useState(initialTime);
-  const timerRef = useRef<number | null>(null);
   const [isRunning, setIsRunning] = useState(false);
+
+  const startTimeRef = useRef<number | null>(null);
+  const pausedTimeRef = useRef<number>(0);
+  const timerRef = useRef<number | null>(null);
 
   useEffect(() => {
     return () => {
@@ -25,45 +30,57 @@ export const useTimer = (initialTime: number, handler: THandler) => {
   }, []);
 
   const tick = useCallback(() => {
-    setTime((prevTime) => {
-      const newTime = prevTime - 1000;
-      if (newTime <= 0) {
-        onFinish?.();
-        stop();
-        return 0;
-      }
-      return newTime;
-    });
-  }, []);
+    if (startTimeRef.current === null) return;
+
+    const now = Date.now();
+    const elapsedTime = now - startTimeRef.current + pausedTimeRef.current;
+    const newTime = Math.max(0, initialTime - elapsedTime); // clamp
+
+    setTime(newTime);
+
+    if (newTime <= 0) {
+      onFinish?.();
+      stop();
+    }
+  }, [initialTime, onFinish]);
 
   const start = useCallback(() => {
-    if (isRunning) {
-      return;
-    }
-    timerRef.current = window.setInterval(tick, 1000);
+    if (isRunning) return;
+
+    startTimeRef.current = Date.now();
+    timerRef.current = window.setInterval(tick, INTERVAL_MS);
+
     setIsRunning(true);
     onStart?.();
   }, [tick, onStart, isRunning]);
 
   const stop = useCallback(() => {
-    setTime(initialTime);
-    _clearInterval();
     setIsRunning(false);
+    setTime(initialTime);
+
+    _clearInterval();
+    startTimeRef.current = null;
+    pausedTimeRef.current = 0;
+
     onStop?.();
-  }, [onStop, isRunning]);
+  }, [initialTime, onStop]);
 
   const pause = useCallback(() => {
-    if (!isRunning) {
-      return;
-    }
-    _clearInterval();
-    setIsRunning(false);
+    if (!isRunning) return;
 
+    _clearInterval();
+    // @note: 타이머가 시작된 적이 있다면, 현재까지 경과된 시간을 계산하여 누적
+    if (startTimeRef.current !== null) {
+      pausedTimeRef.current += Date.now() - startTimeRef.current;
+    }
+
+    setIsRunning(false);
     onPause?.();
-  }, [stop, onPause, isRunning]);
+  }, [onPause, isRunning]);
 
   const _clearInterval = () => {
     if (timerRef.current === null) return;
+
     clearInterval(timerRef.current);
     timerRef.current = null;
   };
