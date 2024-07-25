92bb89d9164deaaa57ceda0ce9b7b01404e98bb0
young-do <yd199323@gmail.com>
2024-07-25 23:48:58 +0900

    hook 잘못 수정한것도 겸사겸사 수정

--- src/renderer/shared/hooks/rive.ts
+++ src/renderer/shared/hooks/rive.ts
@@ -5,15 +5,15 @@ import { useStateMachineInput } from '@rive-app/react-canvas';
 // @note: useStateMachineInput의 반환값에 value를 할당해도 리렌더링이 일어나지 않아 커스텀 훅을 만듬
 export const useRiveStateMachineInput = (...args: Parameters<typeof useStateMachineInput>) => {
   const [rive, stateMachineName, inputName, initialValue] = args;
-  const [value, _setValue] = useState(initialValue);
-  const input = useStateMachineInput(rive, stateMachineName, inputName, value);
+  const [_value, _setValue] = useState(initialValue);
+  const input = useStateMachineInput(rive, stateMachineName, inputName, _value);
 
   const setValue = (newValue: number) => {
-    if (input && input.value !== value) {
+    if (input && input.value !== newValue) {
       input.value = newValue;
       _setValue(input.value);
     }
   };
 
-  return [value, setValue, input] as const;
+  return [_value, setValue, input] as const;
 };
