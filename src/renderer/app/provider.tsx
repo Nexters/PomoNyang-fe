85378c28c9e1d7386bfa8594f73d623493249f5c
Young-do Cho <yd199323@gmail.com>
2024-07-27 15:33:32 +0900

    동일 뮤테이션을 가장 마지막것만 보내고 요청 데이터도 수정될 수 있게 구현 (#14)

--- src/renderer/app/provider.tsx
+++ src/renderer/app/provider.tsx
@@ -1,60 +1,74 @@
 import { PropsWithChildren } from 'react';
 
 import { createSyncStoragePersister } from '@tanstack/query-sync-storage-persister';
-import { QueryClient } from '@tanstack/react-query';
+import { dehydrate, Mutation, QueryClient } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import {
-  // PersistedClient,
-  PersistQueryClientProvider,
-  // PersistRetryer,
-} from '@tanstack/react-query-persist-client';
+import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client';
+import type { PersistedClient } from '@tanstack/react-query-persist-client';
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
+      // 디바이스와 서버간 데이터는 달라질 일이 거의 없을거라고 가정하여, 캐시를 무제한으로 설정
+      gcTime: Infinity,
+      staleTime: Infinity,
       retry: 0,
-      staleTime: 1000 * 60 * 60, // 60분
     },
   },
 });
 
-// 가장 최신의 mutation만 유지하는 함수
-// const keepLatestMutations: PersistRetryer = ({ persistedClient }) => {
-//   const mutations = [...persistedClient.clientState.mutations];
-//   const queries = [...persistedClient.clientState.queries];
-
-//   // mutationKey를 기준으로 가장 최신의 mutation만 유지
-//   const latestMutations = mutations.reduce(
-//     (acc, mutation) => {
-//       const key = JSON.stringify(mutation.mutationKey);
-//       acc[key] = mutation;
-//       return acc;
-//     },
-//     {} as Record<string, (typeof mutations)[0]>,
-//   );
-
-//   const client: PersistedClient = {
-//     ...persistedClient,
-//     clientState: {
-//       mutations: Object.values(latestMutations),
-//       queries,
-//     },
-//   };
-//   return client;
-// };
-
 const persister = createSyncStoragePersister({
   storage: window.localStorage,
-  // serialize: (client) => {
-  //   const optimizedClient = keepLatestMutations({
-  //     persistedClient: client as PersistedClient,
-  //     error: new Error(),
-  //     errorCount: 0,
-  //   });
+  serialize: (persistedClient) => {
+    const mutationCache = queryClient.getMutationCache();
+    const pausedMutations = mutationCache.getAll().filter((mutation) => mutation.state.isPaused);
 
-  //   console.log('serialize: ', optimizedClient || client);
-  //   return JSON.stringify(optimizedClient || client);
-  // },
+    // 가장 최신의 mutation만 map으로 저장
+    const lastMutationMap = new Map<string, Mutation>();
+    pausedMutations.forEach((mutation) => {
+      const key = JSON.stringify(mutation.options.mutationKey);
+      if (!lastMutationMap.has(key)) {
+        return lastMutationMap.set(key, mutation);
+      }
+      const latestMutation = lastMutationMap.get(key) as Mutation;
+      if (mutation.state.submittedAt > latestMutation.state.submittedAt) {
+        return lastMutationMap.set(key, mutation);
+      }
+    });
+    // 최신이 아닌 mutation은 mutationCache에서 제거
+    pausedMutations.forEach((mutation) => {
+      const key = JSON.stringify(mutation.options.mutationKey);
+      if (lastMutationMap.get(key) !== mutation) {
+        return mutationCache.remove(mutation);
+      }
+    });
+    // 최신 Mutation들의 variables 수정
+    // TODO: mutation key로 분기해서 개별로 관리하기
+    // ex) 특정 mutation은 마지막 한번만 보내거나,
+    //     또 보내더라도 요청 variables를 기존것까지 합쳐서 보내거나
+    lastMutationMap.forEach((mutation) => {
+      // @note: 객체를 직접 수정하면 수정된 데이터가 보내짐.
+      // 그러나 새로운 객체를 할당하면 수정전 원본 데이터가 보내진다.
+      // 그래서 variables 객체를 직접 수정해야함...!
+
+      // const prevVariables = mutation.state.variables as { title: string; id: number };
+      // const nextVariables = { ...prevVariables, title: 'modified' };
+      (mutation.state.variables as { title: string; id: number }).title = 'modified';
+    });
+
+    // queryClient로 persistedClient 새로 생성하기...
+    // @see: https://github.com/TanStack/query/blob/v5.51.4/packages/query-persist-client-core/src/persist.ts#L117-L121
+    const newPersistClient: PersistedClient = {
+      buster: persistedClient.buster,
+      timestamp: persistedClient.timestamp,
+      // note: 두번째 인자인 DehydrateOptions은 PersistQueryClientProvider > persistOptions 에 준 값으로 할당해야함
+      // 여기선 할당하지 않았으니, undefined로 할당함
+      clientState: dehydrate(queryClient, undefined),
+    };
+    console.log('serialize: ', newPersistClient, mutationCache.getAll());
+
+    return JSON.stringify(newPersistClient);
+  },
 });
 
 export const Provider = ({ children }: PropsWithChildren) => {
