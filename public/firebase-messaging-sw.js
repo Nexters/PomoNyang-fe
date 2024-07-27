fe34c97f35c7f2d3cacab568d8431722971955c3
leeharyung <lhr4884@naver.com>
2024-07-27 23:09:42 +0900

    firebase-messaging-sw 추가 & 주석처리

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,28 @@
+/* eslint-disable no-undef */
+// /public/firebase-messaging-sw.js
+// importScripts('https://www.gstatic.com/firebasejs/9.1.1/firebase-app-compat.js');
+// importScripts('https://www.gstatic.com/firebasejs/9.1.1/firebase-messaging-compat.js');
+
+// const firebaseConfig = {
+//   apiKey: 'AIzaSyAcSgJCbPv7TW3nEKe2J9Hl9keqjxnS9To',
+//   authDomain: 'pomonyang.firebaseapp.com',
+//   projectId: 'pomonyang',
+//   storageBucket: 'pomonyang.appspot.com',
+//   messagingSenderId: '283667420790',
+//   appId: '1:283667420790:web:711297425eb2a4b7227157',
+//   measurementId: 'G-43Y9KDES2S',
+// };
+
+// firebase.initializeApp(firebaseConfig);
+
+// const messaging = firebase.messaging();
+
+// messaging.onBackgroundMessage(function (payload) {
+//   console.log('[firebase-messaging-sw.js] Received background message ', payload);
+//   const notificationTitle = 'Background Message Title';
+//   const notificationOptions = {
+//     body: 'Background Message body.',
+//   };
+
+//   self.registration.showNotification(notificationTitle, notificationOptions);
+// });
