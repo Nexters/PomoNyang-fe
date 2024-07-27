3e1e329a639c28a5a82b923f27944321f1e19b8a
leeharyung <lhr4884@naver.com>
2024-07-27 23:09:08 +0900

    firebase env 추가

--- src/renderer/vite-env.d.ts
+++ src/renderer/vite-env.d.ts
@@ -7,6 +7,16 @@ declare module '*.riv';
 // @see: https://electron-vite.org/guide/env-and-mode
 interface ImportMetaEnv {
   readonly VITE_SAMPLE: string;
+
+  // firebase
+  readonly VITE_FIREBASE_VAPID_KEY: string;
+  readonly VITE_FIREBASE_API_KEY: string;
+  readonly VITE_FIREBASE_AUTH_DOMAIN: string;
+  readonly VITE_FIREBASE_PROJECT_ID: string;
+  readonly VITE_FIREBASE_STORAGE_BUCKET: string;
+  readonly VITE_FIREBASE_MESSAGING_SENDER_ID: string;
+  readonly VITE_FIREBASE_APP_ID: string;
+  readonly VITE_FIREBASE_MEASUREMENT_ID: string;
 }
 
 interface ImportMeta {
