d720dbc9b322d51247b4371b998fb4349b5992ec
Young-do Cho <yd199323@gmail.com>
2024-07-27 13:47:10 +0900

    feat: .env 파일 지원하기 (#13)
    * import.meta 타입 지원을 위한 설정
    
    * hook 잘못 수정한것도 겸사겸사 수정

--- src/renderer/vite-env.d.ts
+++ src/renderer/vite-env.d.ts
@@ -3,9 +3,12 @@
 // modules
 declare module '*.riv';
 
-// electron api
-declare global {
-  interface Window {
-    electronAPI: import('../shared/type').IElectronAPI;
-  }
+// vite env
+// @see: https://electron-vite.org/guide/env-and-mode
+interface ImportMetaEnv {
+  readonly VITE_SAMPLE: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
 }
