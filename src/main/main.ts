bdd4ebedb1405ea518c5640b6a0f233d17e85447
leeharyung <lhr4884@naver.com>
2024-07-28 18:09:13 +0900

    window창 width, height 설정

--- src/main/main.ts
+++ src/main/main.ts
@@ -11,8 +11,11 @@ if (require('electron-squirrel-startup')) {
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    minWidth: 400,
+
+    width: 400,
+    height: 800,
+
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
     },
