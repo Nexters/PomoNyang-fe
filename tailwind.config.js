563e7724e2b0e55327209daa9d82a94238ad601e
leeharyung <lhr4884@naver.com>
2024-07-28 18:07:53 +0900

    pretendard 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,9 @@ module.exports = {
       },
     },
     extend: {
+      fontFamily: {
+        pretendard: ['Pretendard Variable', 'sans-serif'],
+      },
       colors: {
         border: 'hsl(var(--border))',
         input: 'hsl(var(--input))',
