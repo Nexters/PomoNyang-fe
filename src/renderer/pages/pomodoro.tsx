4b3ea66cf693c80976375e83c1f13e73f9c9624d
Young-do Cho <yd199323@gmail.com>
2024-07-29 22:15:31 +0900

    feat: drawer (+ toast) 컴포넌트 추가 (#15)
    * drawer 추가
    
    * toast도 겸사겸사 추가

--- src/renderer/pages/pomodoro.tsx
+++ src/renderer/pages/pomodoro.tsx
@@ -1,13 +1,16 @@
+import { useState } from 'react';
+
 import { useNavigate } from 'react-router-dom';
 
 import { PATH } from '@/shared/constants';
 import { useTimer } from '@/shared/hooks';
-import { Button } from '@/shared/ui';
+import { Button, Drawer, DrawerContent, DrawerFooter, DrawerTitle, useToast } from '@/shared/ui';
 
 const INITIAL_TIME = 1000 * 60 * 25;
 
 const Pomodoro = () => {
   const navigate = useNavigate();
+  const { toast } = useToast();
   const { time, isRunning, start, resume, stop, pause } = useTimer(INITIAL_TIME, {
     onFinish: () => {
       new Notification('모하냥', {
@@ -15,24 +18,53 @@ const Pomodoro = () => {
       });
     },
   });
+  const [isOpenDrawer, setIsOpenDrawer] = useState(false);
 
   return (
-    <div>
-      <Button
-        onClick={() => {
-          navigate(PATH.HOME);
-        }}
-      >
-        홈 페이지로 가기
-      </Button>
-      <h1>뽀모도로 타이머</h1>
-      {Math.floor(time / 1000)} : {INITIAL_TIME / 1000}
-      <div>isRunning: {isRunning ? 'true' : 'false'}</div>
-      <Button onClick={start}>START</Button>
-      <Button onClick={resume}>RESUME</Button>
-      <Button onClick={pause}>PAUSE</Button>
-      <Button onClick={stop}>STOP</Button>
-    </div>
+    <Drawer open={isOpenDrawer} onOpenChange={setIsOpenDrawer}>
+      <div>
+        <Button
+          onClick={() => {
+            navigate(PATH.HOME);
+          }}
+        >
+          홈 페이지로 가기
+        </Button>
+        <h1>뽀모도로 타이머</h1>
+        {Math.floor(time / 1000)} : {INITIAL_TIME / 1000}
+        <div>isRunning: {isRunning ? 'true' : 'false'}</div>
+        <Button onClick={start}>START</Button>
+        <Button onClick={resume}>RESUME</Button>
+        <Button onClick={pause}>PAUSE</Button>
+        <Button onClick={stop}>STOP</Button>
+        <Button onClick={() => setIsOpenDrawer(true)}>끝났냥</Button>
+        <DrawerContent>
+          <div className="p-4">
+            <DrawerTitle>집중 시간 어땠옹?</DrawerTitle>
+            <p>나중에 설정에서 선택을 변경할 수 있엉</p>
+
+            <div className="flex flex-col gap-2">
+              <Button size="lg">쫌 짧드라</Button>
+              <Button size="lg">적당했어</Button>
+              <Button size="lg">쫌 길드라</Button>
+            </div>
+          </div>
+          <DrawerFooter>
+            <Button
+              size="lg"
+              onClick={() => {
+                setIsOpenDrawer(false);
+                toast({
+                  title: '오키 5분 추가했엉 다음 집중때 해바',
+                });
+              }}
+            >
+              완료
+            </Button>
+          </DrawerFooter>
+        </DrawerContent>
+      </div>
+    </Drawer>
   );
 };
 
--- src/renderer/shared/ui/index.ts
+++ src/renderer/shared/ui/index.ts
@@ -1,2 +1,6 @@
 export * from './button';
 export * from './carousel';
+export * from './drawer';
+export * from './toast';
+export * from './toaster';
+export * from './use-toast';
