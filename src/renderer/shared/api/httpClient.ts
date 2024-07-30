e374afebb3bf3c6e4b79014cdffd4345ed99a8e7
young-do <yd199323@gmail.com>
2024-07-30 22:17:57 +0900

    요청 바디 및 응답 타입 자동 추론되도록

--- src/renderer/shared/api/httpClient.ts
+++ src/renderer/shared/api/httpClient.ts
@@ -4,13 +4,13 @@ import { resolveUrl } from '@/shared/utils';
 export const BASE_URL = '';
 
 export const httpClient = {
-  get: <T>(url: string) => __fetch<T, void>('get', url),
-  post: <T, D>(url: string, body: D) => __fetch<T, D>('post', url, body),
-  put: <T, D>(url: string, body: D) => __fetch<T, D>('put', url, body),
-  delete: <T>(url: string) => __fetch<T, void>('delete', url),
+  get: <T = unknown>(url: string) => __fetch<T, void>('get', url),
+  post: <T = unknown, D = unknown>(url: string, body: D) => __fetch<T, D>('post', url, body),
+  put: <T = unknown, D = unknown>(url: string, body: D) => __fetch<T, D>('put', url, body),
+  delete: <T = unknown>(url: string) => __fetch<T, void>('delete', url),
 };
 
-const __fetch = async <T, D>(method: string, path: string, body?: D) => {
+const __fetch = async <T = unknown, D = unknown>(method: string, path: string, body?: D) => {
   const response = await fetch(resolveUrl(BASE_URL, path), {
     method,
     // @note: body가 undefined이면, stringify 된 값도 undefined이므로 body는 전송되지 않는다.
