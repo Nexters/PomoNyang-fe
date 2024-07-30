6936c3c14175f27edd9e35dd0526e81f184b67bb
halang <lhr4884@naver.com>
2024-07-30 22:57:32 +0900

    feat: layout 및 pretendard 글꼴 추가 (#17)
    * pretendard 추가
    
    * window창 width, height 설정
    
    * layout 추가
    
    * 윈도우 크기 고정 제거
    
    * 파일명 _ 삭제
    
    * import 수정
    
    * max-w-md 로 수정

--- src/main/main.ts
+++ src/main/main.ts
@@ -11,8 +11,6 @@ if (require('electron-squirrel-startup')) {
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
     },
