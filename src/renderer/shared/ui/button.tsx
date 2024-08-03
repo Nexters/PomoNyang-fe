837a9aaaf9fec05864eb600c77736b9646fd17d3
leeharyung <lhr4884@naver.com>
2024-08-03 15:23:05 +0900

    버튼 컴포넌트 추가

--- src/renderer/shared/ui/button.tsx
+++ src/renderer/shared/ui/button.tsx
@@ -6,22 +6,23 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import { cn } from '@/shared/utils';
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
+  'inline-flex gap-[8px] items-center justify-center whitespace-nowrap ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
   {
     variants: {
       variant: {
-        default: 'bg-primary text-primary-foreground hover:bg-primary/90',
-        destructive: 'bg-destructive text-destructive-foreground hover:bg-destructive/90',
-        outline: 'border border-input bg-background hover:bg-accent hover:text-accent-foreground',
-        secondary: 'bg-secondary text-secondary-foreground hover:bg-secondary/80',
-        ghost: 'hover:bg-accent hover:text-accent-foreground',
-        link: 'text-primary underline-offset-4 hover:underline',
+        default:
+          'bg-bg-accent-1 text-text-inverse disabled:bg-bg-secondary disabled:text-text-disabled', // primary
+        secondary: 'bg-bg-inverse text-text-inverse',
+        tertiary: 'bg-bg-secondary text-text-tertiary',
+        'text-primary': 'bg-transparent text-text-secondary',
+        'text-secondary': 'bg-transparent text-text-tertiary',
+        icon: 'bg-bg-inverse',
       },
       size: {
-        default: 'h-10 px-4 py-2',
-        sm: 'h-9 rounded-md px-3',
-        lg: 'h-11 rounded-md px-8',
-        icon: 'h-10 w-10',
+        default: 'rounded-sm p-[16px] body-sb', // md
+        sm: 'rounded-xs px-[12px] py-[8px] subBody-sb',
+        lg: 'rounded-sm p-[20px] header-5',
+        round: 'rounded-full p-[28px] w-fit h-fit',
       },
     },
     defaultVariants: {
