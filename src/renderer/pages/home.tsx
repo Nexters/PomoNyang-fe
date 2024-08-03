c9848306c74d66c59fb61117329141a60fe64df5
leeharyung <lhr4884@naver.com>
2024-08-03 11:23:10 +0900

    타이포 샘플 추가

--- src/renderer/pages/home.tsx
+++ src/renderer/pages/home.tsx
@@ -40,6 +40,24 @@ const Home = () => {
       >
         뽀모도로 페이지로 가기
       </Button>
+      <div className="container">
+        <div className="time">time</div>
+
+        <div className="header-1">header-1</div>
+        <div className="header-2">header-2</div>
+        <div className="header-3">header-3</div>
+        <div className="header-4">header-4</div>
+        <div className="header-5">header-5</div>
+
+        <div className="body-sb">body-sb</div>
+        <div className="body-r">body-r</div>
+
+        <div className="subBody-sb">subBody-sb</div>
+        <div className="subBody-r">subBody-r</div>
+
+        <div className="caption-sb">caption-sb</div>
+        <div className="caption-r">caption-r</div>
+      </div>
     </div>
   );
 };
