020bce5eb666491acf50032a0fc22f5119d8ae9b
young-do <yd199323@gmail.com>
2024-08-04 08:59:56 +0900

    auth token 얻은 뒤에 fetch 호출할 수 있는 auth client 추가

--- src/renderer/shared/api/httpClient.ts
+++ src/renderer/shared/api/httpClient.ts
@@ -2,15 +2,27 @@ import { resolveUrl } from '@/shared/utils';
 
 export const BASE_URL = import.meta.env.VITE_API_SERVER_URL ?? '';
 
-export const httpClient = {
-  get: <T = unknown>(url: string, headers?: object) => __fetch<T, void>('get', url, { headers }),
-  post: <T = unknown, D = unknown>(url: string, body: D, headers?: object) =>
-    __fetch<T, D>('post', url, { body, headers }),
-  put: <T = unknown, D = unknown>(url: string, body: D, headers?: object) =>
-    __fetch<T, D>('put', url, { body, headers }),
-  delete: <T = unknown>(url: string, headers?: object) =>
-    __fetch<T, void>('delete', url, { headers }),
+export const createHttpClient = (defaultHeader?: object) => {
+  const mergeHeaders = (headers?: object) => {
+    return {
+      ...defaultHeader,
+      ...headers,
+    };
+  };
+
+  return {
+    get: <T = unknown>(url: string, headers?: object) =>
+      __fetch<T, void>('get', url, { headers: mergeHeaders(headers) }),
+    post: <T = unknown, D = unknown>(url: string, body: D, headers?: object) =>
+      __fetch<T, D>('post', url, { body, headers: mergeHeaders(headers) }),
+    put: <T = unknown, D = unknown>(url: string, body: D, headers?: object) =>
+      __fetch<T, D>('put', url, { body, headers: mergeHeaders(headers) }),
+    delete: <T = unknown>(url: string, headers?: object) =>
+      __fetch<T, void>('delete', url, { headers: mergeHeaders(headers) }),
+  };
 };
+
+export const httpClient = createHttpClient();
 
 const __fetch = async <T = unknown, D = unknown>(
   method: string,
--- src/renderer/shared/hooks/index.ts
+++ src/renderer/shared/hooks/index.ts
@@ -2,3 +2,4 @@ export * from './rive';
 export * from './machine-id';
 export * from './use-timer';
 export * from './use-auth-token';
+export * from './use-auth-client';
