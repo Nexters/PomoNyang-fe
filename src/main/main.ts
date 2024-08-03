7cb112d7492c4582dc06962a19448d57517ce7c8
young-do <yd199323@gmail.com>
2024-08-03 15:57:03 +0900

    cors 더욱 간단하게 해결

--- src/main/main.ts
+++ src/main/main.ts
@@ -3,8 +3,6 @@ import path from 'path';
 
 import { machineId } from 'node-machine-id';
 
-import { ignoreCors } from './cors';
-
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
   app.quit();
@@ -17,11 +15,9 @@ const createWindow = () => {
     height: 600,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
+      webSecurity: false,
     },
   });
-
-  // set up
-  ignoreCors(mainWindow);
 
   // and load the index.html of the app.
   if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
