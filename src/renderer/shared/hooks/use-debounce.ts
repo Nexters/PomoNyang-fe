4f54faf55aea2c285caf2d7919abe9d0a8da6e6f
leeharyung <lhr4884@naver.com>
2024-08-08 00:33:28 +0900

    useDebounce 훅 추가

---
+++ src/renderer/shared/hooks/use-debounce.ts
@@ -0,0 +1,17 @@
+import { useRef } from 'react';
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+// 모든 타입의 인자를 다 받을 수 있으며 어떠한 타입으로도 반환될 수 있음
+type TDebounceFunction<T extends any[]> = (...args: T) => any;
+
+export const useDebounce = <T extends any[]>(callback: TDebounceFunction<T>, delay: number) => {
+  const timeoutRef = useRef<NodeJS.Timeout>();
+
+  // 디바운싱된 함수를 반환
+  // callback 함수와 동일한 인자를 받아서 delay 시간이 지난 후에 callback 함수를 실행
+  return function debounced(...arg: T) {
+    clearTimeout(timeoutRef.current);
+    timeoutRef.current = setTimeout(() => callback(...arg), delay);
+  };
+};
