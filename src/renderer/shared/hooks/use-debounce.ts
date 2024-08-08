e480a6641cd32c64a77c5d427f993701db50d354
leeharyung <lhr4884@naver.com>
2024-08-08 21:57:32 +0900

    any 대신 unknown으로 수정

--- src/renderer/shared/hooks/use-debounce.ts
+++ src/renderer/shared/hooks/use-debounce.ts
@@ -1,11 +1,9 @@
 import { useRef } from 'react';
 
-/* eslint-disable @typescript-eslint/no-explicit-any */
-
 // 모든 타입의 인자를 다 받을 수 있으며 어떠한 타입으로도 반환될 수 있음
-type TDebounceFunction<T extends any[]> = (...args: T) => any;
+type TDebounceFunction<T extends unknown[]> = (...args: T) => unknown;
 
-export const useDebounce = <T extends any[]>(callback: TDebounceFunction<T>, delay: number) => {
+export const useDebounce = <T extends unknown[]>(callback: TDebounceFunction<T>, delay: number) => {
   const timeoutRef = useRef<NodeJS.Timeout>();
 
   // 디바운싱된 함수를 반환
