037a17aa15489b843e8b22d08688762017089345
leeharyung <lhr4884@naver.com>
2024-08-11 18:22:09 +0900

    주석 수정

--- src/renderer/shared/utils/format-duration.ts
+++ src/renderer/shared/utils/format-duration.ts
@@ -1,14 +1,12 @@
 export const formatDuration = (isoDuration: string) => {
   const regex = /P(?:(\d+)Y)?(?:(\d+)M)?(?:(\d+)D)?(?:T(?:(\d+)H)?(?:(\d+)M)?(?:(\d+)S)?)?/;
 
-  // Parse the ISO 8601 duration string
   const matches = isoDuration.match(regex);
 
   if (!matches) {
     throw new Error('Invalid ISO 8601 duration format');
   }
 
-  // Extract the components or use 0 if they are undefined
   const years = matches[1] || 0;
   const months = matches[2] || 0;
   const days = matches[3] || 0;
