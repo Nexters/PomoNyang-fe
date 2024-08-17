9eaac69593781a3ba9a2c83cfdc1c4131935f346
leeharyung <lhr4884@naver.com>
2024-08-17 20:34:37 +0900

    useAddPomodoro 추가

---
+++ src/renderer/features/pomodoro/hooks/use-add-pomodoro.ts
@@ -0,0 +1,21 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+
+import { Pomodoro } from '@/entities/pomodoro';
+import { useAuthClient } from '@/shared/hooks';
+
+type AddPomodoroBody = Pomodoro[];
+
+export const useAddPomodoro = () => {
+  const queryClient = useQueryClient();
+  const authClient = useAuthClient();
+  return useMutation({
+    mutationFn: async ({ body }: { body: AddPomodoroBody }) => {
+      return await authClient?.post<void, AddPomodoroBody>('/api/v1/focus-times', body);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['pomodoro'],
+      });
+    },
+  });
+};
