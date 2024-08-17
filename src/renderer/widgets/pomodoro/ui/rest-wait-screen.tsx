ee56a1d55027d1a47c245148841405c758c8dc3c
leeharyung <lhr4884@naver.com>
2024-08-18 03:15:04 +0900

    휴식 대기 페이지 수정

--- src/renderer/widgets/pomodoro/ui/rest-wait-screen.tsx
+++ src/renderer/widgets/pomodoro/ui/rest-wait-screen.tsx
@@ -1,39 +1,47 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 
+import { PomodoroNextAction } from '@/entities/pomodoro';
 import { CategoryChip } from '@/features/category/ui/category-chip';
 import { Time } from '@/features/time';
+import { MAX_FOCUS_MINUTES, MIN_FOCUS_MINUTES, MINUTES_GAP } from '@/shared/constants';
 import { useTimer } from '@/shared/hooks';
-import { Button } from '@/shared/ui';
+import { Button, Icon, SelectGroup, SelectGroupItem } from '@/shared/ui';
 import { msToTime } from '@/shared/utils';
 
 type RestWaitScreenProps = {
   currentCategory: string;
+  currentFocusMinutes: number;
   time: number;
   handleRest: () => void;
   handleEnd: () => void;
   handleInit: () => void;
+  selectedNextAction: PomodoroNextAction | undefined;
+  setSelectedNextAction: (nextAction: PomodoroNextAction) => void;
 };
 
 // @TODO: 60분으로 수정
-const MAX_TIME_ON_PAGE = 1000 * 60;
+const MAX_TIME_ON_PAGE = 1000 * 5; // 5초
 
 export const RestWaitScreen = ({
   currentCategory,
+  currentFocusMinutes,
   time,
   handleRest,
   handleEnd,
   handleInit,
+  selectedNextAction,
+  setSelectedNextAction,
 }: RestWaitScreenProps) => {
   const { minutes, seconds } = msToTime(time > 0 ? time : 0);
   const { minutes: exceedMinutes, seconds: exceedSeconds } = msToTime(time < 0 ? -time : 0);
 
-  const { start, stop } = useTimer(MAX_TIME_ON_PAGE, 0, {
-    onFinish: () => {
-      handleInit();
-    },
-  });
+  const onFinishRef = useRef(handleInit);
 
   const isExceed = time < 0;
+
+  const { start, stop } = useTimer(MAX_TIME_ON_PAGE, 0, {
+    onFinishRef,
+  });
 
   useEffect(() => {
     start();
@@ -46,7 +54,7 @@ export const RestWaitScreen = ({
   return (
     <div className="flex flex-col h-full">
       <header className="h-[56px]" />
-      <main className="flex flex-col items-center gap-xl">
+      <main className="flex flex-col items-center justify-center flex-1 gap-5">
         <div className="flex flex-col items-center justify-center gap-sm">
           <CategoryChip category={currentCategory} />
           <Time minutes={minutes} seconds={seconds} className="header-1 text-text-primary gap-xs" />
@@ -62,9 +70,33 @@ export const RestWaitScreen = ({
           )}
         </div>
         <div className="w-[240px] h-[240px] bg-background-secondary" />
-        <div>다음부터 집중 시간을 바꿀까요?</div>
+        <div className="flex flex-col gap-3">
+          <p className="body-sb text-text-disabled">다음부터 집중시간을 바꿀까요?</p>
+          <SelectGroup
+            className="flex"
+            value={selectedNextAction}
+            onValueChange={setSelectedNextAction}
+          >
+            <SelectGroupItem
+              disabled={currentFocusMinutes - MINUTES_GAP <= MIN_FOCUS_MINUTES}
+              value="minus"
+              className="flex flex-row items-center justify-center gap-1 px-3 py-2"
+            >
+              <Icon name="minusSvg" size="sm" className="[&>path]:stroke-icon-tertiary" />
+              <span className="body-sb text-text-tertiary">5분</span>
+            </SelectGroupItem>
+            <SelectGroupItem
+              disabled={currentFocusMinutes + MINUTES_GAP >= MAX_FOCUS_MINUTES}
+              value="plus"
+              className="flex flex-row items-center justify-center gap-1 px-3 py-2"
+            >
+              <Icon name="plusSvg" size="sm" className="[&>path]:stroke-icon-tertiary" />
+              <span className="body-sb text-text-tertiary">5분</span>
+            </SelectGroupItem>
+          </SelectGroup>
+        </div>
       </main>
-      <div className="absolute left-0 flex flex-col items-center w-full m-auto bottom-4">
+      <div className="flex flex-col items-center w-full pb-5">
         <Button variant="primary" className="p-xl w-[200px]" size="lg" onClick={handleRest}>
           휴식 시작하기
         </Button>
