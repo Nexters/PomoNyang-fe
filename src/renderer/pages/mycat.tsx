ec6a769bb745c4f0d23e74e5c6dff21624320b54
young-do <yd199323@gmail.com>
2024-08-20 13:54:03 +0900

    나의 고양이는 선택페이지꺼 활용

--- src/renderer/pages/mycat.tsx
+++ src/renderer/pages/mycat.tsx
@@ -1,6 +1,11 @@
+import { useEffect } from 'react';
+
+import { useRive } from '@rive-app/react-canvas';
 import { useNavigate } from 'react-router-dom';
 
+import { CatType } from '@/entities/cat';
 import { useUser } from '@/features/user';
+import catSelectMotionRiveFile from '@/shared/assets/rivs/cat_select_ver2.0.riv';
 import { PATH } from '@/shared/constants';
 import { Button, Frame, Icon, Tooltip } from '@/shared/ui';
 
@@ -15,6 +20,29 @@ const MyCat = () => {
     navigate(PATH.SELECTION, { state: { fromMyCatPage: true } });
   };
 
+  const { rive, RiveComponent } = useRive({
+    src: catSelectMotionRiveFile,
+    stateMachines: 'State Machine_selectCat',
+    autoplay: true,
+  });
+
+  useEffect(() => {
+    const userCatType = user?.cat?.type;
+    if (!rive || !userCatType) return;
+
+    // 선택페이지 고양이가 자동으로 선택되도록 설정
+    const userCatTypeAliasMap: Record<CatType, string> = {
+      CHEESE: 'cheese',
+      BLACK: 'black',
+      THREE_COLOR: 'calico',
+    };
+    const userCatTypeAlias = userCatTypeAliasMap[userCatType];
+    const clickCatInput = rive.stateMachineInputs('State Machine_selectCat').find((input) => {
+      return input.name.toLowerCase().includes(userCatTypeAlias);
+    });
+    clickCatInput?.fire();
+  }, [rive, user?.cat?.type]);
+
   return (
     <Frame>
       <Frame.NavBar title="나의 고양이" onBack={() => navigate(PATH.MY_PAGE)} />
@@ -27,9 +55,9 @@ const MyCat = () => {
             sideOffset={-20}
             rootProps={{ open: true }}
             arrowProps={{ width: 14, height: 9 }}
-          >
-            <div className=" h-[240px] bg-background-secondary" />
-          </Tooltip>
+          />
+
+          <RiveComponent className="w-full h-[240px]" />
 
           <div className="w-full flex justify-center items-center">
             <button
