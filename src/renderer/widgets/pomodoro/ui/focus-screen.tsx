8120c77e3c4b8626af12847a3f6ca491b5126cee
leeharyung <lhr4884@naver.com>
2024-08-22 01:26:36 +0900

    집중 페이지 > 휴식하기 버튼 색 변하도록

--- src/renderer/widgets/pomodoro/ui/focus-screen.tsx
+++ src/renderer/widgets/pomodoro/ui/focus-screen.tsx
@@ -69,7 +69,12 @@ export const FocusScreen = ({ currentCategory, time, handleRest, handleEnd }: Fo
         </div>
       </main>
       <div className="absolute left-0 flex flex-col items-center w-full m-auto bottom-4">
-        <Button variant="secondary" className="p-xl w-[200px]" size="lg" onClick={handleRest}>
+        <Button
+          variant={isExceed ? 'primary' : 'secondary'}
+          className="p-xl w-[200px]"
+          size="lg"
+          onClick={handleRest}
+        >
           휴식하기
         </Button>
         <Button variant="text-secondary" size="md" onClick={handleEnd}>
