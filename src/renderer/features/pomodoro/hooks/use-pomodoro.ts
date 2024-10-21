952cadbbcb4577e3817eba8c2acda8732d24d4c3
young-do <yd199323@gmail.com>
2024-10-22 00:24:28 +0900

    계산된 지난시간, 초과시간이 특정 범위를 넘지 못하도록 수정
    추가로 타입에 대한 주석도 추가

--- src/renderer/features/pomodoro/hooks/use-pomodoro.ts
+++ src/renderer/features/pomodoro/hooks/use-pomodoro.ts
@@ -66,8 +66,9 @@ const updateCycles = (cycles: PomodoroCycle[], nextCycle?: PomodoroCycle): Pomod
 
 export const getPomodoroTime = (cycle: PomodoroCycle): PomodoroTime => {
   const now = cycle.endAt ?? Date.now();
-  const elapsed = now - cycle.startAt;
-  const exceeded = elapsed - cycle.goalTime;
+  const maxElapsedTime = cycle.goalTime + cycle.exceedMaxTime;
+  const elapsed = Math.min(maxElapsedTime, now - cycle.startAt);
+  const exceeded = Math.min(cycle.exceedMaxTime, elapsed - cycle.goalTime);
 
   return { elapsed, exceeded };
 };
