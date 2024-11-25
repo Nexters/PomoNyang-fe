c7a30ea357f919a55b6366740fb28ffc3567640a
young-do <yd199323@gmail.com>
2024-11-26 00:23:24 +0900

    시간 변경 모달 스타일 변경

--- src/renderer/shared/ui/dialog.tsx
+++ src/renderer/shared/ui/dialog.tsx
@@ -11,6 +11,7 @@ export type DialogProps = {
   title?: string;
   hasCloseButton?: boolean;
   fullScreen?: boolean;
+  contentClassname?: string;
   animated?: boolean;
   children: React.ReactNode;
   onOpenChange?: (open: boolean) => void;
@@ -22,6 +23,7 @@ export const Dialog = ({
   hasCloseButton = true,
   fullScreen = false,
   animated = true,
+  contentClassname,
   children,
   onOpenChange,
 }: DialogProps) => {
@@ -42,6 +44,7 @@ export const Dialog = ({
             !fullScreen &&
               'fixed left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] rounded-md p-[20px] shadow-lg',
             'z-50 w-[95%] max-w-md bg-background-primary',
+            contentClassname,
 
             animated &&
               'duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%]',
