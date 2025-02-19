2d002c895f93ce67abb15f782d351f4b057e2956
young-do <yd199323@gmail.com>
2025-02-19 22:10:57 +0900

    카테고리 선택 ui 변경

--- src/renderer/features/category/ui/change-category-drawer.tsx
+++ src/renderer/features/category/ui/change-category-drawer.tsx
@@ -1,18 +1,17 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { useEffect, useState } from 'react';
 
 import { useCategories } from '@/features/category';
 import {
-  Button,
   Icon,
   Drawer,
   DrawerContent,
   DrawerTitle,
-  DrawerFooter,
-  DrawerClose,
   SelectGroup,
   SelectGroupItem,
+  DrawerDescription,
 } from '@/shared/ui';
-import { getCategoryIconName, parseIsoDuration } from '@/shared/utils';
+import { cn, getCategoryIconName } from '@/shared/utils';
 
 type ChangeCategoryDrawerProps = {
   open: boolean;
@@ -20,6 +19,7 @@ type ChangeCategoryDrawerProps = {
   onOpenChange: (isOpen: boolean) => void;
   onChangeCategory: (category: string) => void;
 };
+type ChangeCategoryDrawerMode = 'select' | 'edit' | 'delete';
 
 export const ChangeCategoryDrawer = ({
   open,
@@ -28,11 +28,66 @@ export const ChangeCategoryDrawer = ({
   onChangeCategory,
 }: ChangeCategoryDrawerProps) => {
   const [selectedCategory, setSelectedCategory] = useState(defaultCategory);
+  const [mode, setMode] = useState<ChangeCategoryDrawerMode>('select');
   const { data: categories } = useCategories();
+  const hasMultipleCategories = !!categories && categories.length > 1;
 
   useEffect(() => {
     setSelectedCategory(defaultCategory);
   }, [defaultCategory]);
+
+  const SelectModeDrawerContent = () => (
+    <>
+      <DrawerTitle asChild>
+        <div className="ml-xl mr-sm flex items-center justify-between gap-2">
+          <h2 className="header-3 py-1">카테고리</h2>
+          <div className="flex gap-2">
+            <button
+              className="h-10 w-10"
+              onClick={() => {
+                // TODO: 카테고리 추가 모달 열기
+              }}
+            >
+              <Icon name="plusSvg" size="md" className="[&>path]:stroke-icon-primary" />
+            </button>
+            <button
+              className="h-10 w-10"
+              onClick={() => {
+                // TODO: 수정/삭제 메뉴 오픈
+              }}
+            >
+              <Icon name="ellipsis" size="md" className="[&>path]:stroke-icon-primary" />
+            </button>
+          </div>
+        </div>
+      </DrawerTitle>
+      <DrawerDescription asChild>
+        <SelectGroup
+          defaultValue={selectedCategory}
+          onValueChange={(value) => {
+            onChangeCategory(value);
+          }}
+          className={cn(
+            'grid min-h-[120px] gap-2 px-4 py-5',
+            hasMultipleCategories ? 'grid-cols-2' : 'grid-cols-1',
+          )}
+        >
+          {categories?.map((category) => (
+            <SelectGroupItem
+              key={category.no}
+              value={category.title}
+              className="flex w-full flex-row items-center justify-start gap-2 p-5"
+            >
+              <Icon name={getCategoryIconName(category.title)} size="sm" />
+              <span className="body-sb text-text-primary">{category.title}</span>
+            </SelectGroupItem>
+          ))}
+        </SelectGroup>
+      </DrawerDescription>
+    </>
+  );
+  const EditModeDrawerContent = () => <></>;
+  const DeleteModeDrawerContent = () => <></>;
 
   return (
     <Drawer
@@ -43,61 +98,9 @@ export const ChangeCategoryDrawer = ({
       }}
     >
       <DrawerContent>
-        <div className="ml-xl mr-sm flex items-center justify-between gap-2">
-          <DrawerTitle className="header-3 py-1">카테고리 변경</DrawerTitle>
-          <DrawerClose className="p-sm">
-            <Icon name="close" size="sm" />
-          </DrawerClose>
-        </div>
-        <SelectGroup
-          defaultValue={selectedCategory}
-          onValueChange={(value) => {
-            setSelectedCategory(value);
-          }}
-          className="mt-lg flex flex-col gap-4 px-lg"
-        >
-          {categories?.map((category) => {
-            const focusTime =
-              parseIsoDuration(category.focusTime).hours * 60 +
-              parseIsoDuration(category.focusTime).minutes;
-            const restTime =
-              parseIsoDuration(category.restTime).hours * 60 +
-              parseIsoDuration(category.restTime).minutes;
-            return (
-              <SelectGroupItem
-                key={category.no}
-                value={category.title}
-                className="flex w-full flex-row items-center justify-start gap-md p-xl"
-              >
-                <div className="flex gap-sm">
-                  <Icon name={getCategoryIconName(category.title)} size="sm" />
-                  <span className="body-sb text-text-primary">{category.title}</span>
-                </div>
-                <div className="subBody-r flex items-center gap-xs text-text-tertiary">
-                  <span>집중 {focusTime}분</span>
-                  <span>|</span>
-                  <span>휴식 {restTime}분</span>
-                </div>
-              </SelectGroupItem>
-            );
-          })}
-        </SelectGroup>
-
-        <DrawerFooter>
-          <Button
-            variant="secondary"
-            className="w-full"
-            size="lg"
-            onClick={() => {
-              if (selectedCategory) {
-                onChangeCategory(selectedCategory);
-              }
-              onOpenChange(false);
-            }}
-          >
-            확인
-          </Button>
-        </DrawerFooter>
+        {mode === 'select' && <SelectModeDrawerContent />}
+        {mode === 'edit' && <EditModeDrawerContent />}
+        {mode === 'delete' && <DeleteModeDrawerContent />}
       </DrawerContent>
     </Drawer>
   );
