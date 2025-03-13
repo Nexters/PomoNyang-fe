5d280e8f4eaf0c0db576e4056d5bdce8949f92e1
young-do <yd199323@gmail.com>
2025-03-13 21:53:58 +0900

    카테고리 타입 확장

--- src/renderer/entities/category/types.ts
+++ src/renderer/entities/category/types.ts
@@ -3,4 +3,8 @@ export type Category = {
   title: string;
   focusTime: string;
   restTime: string;
+  /** 카테고리 노출 순서. 0부터 시작 */
+  position: number;
+  iconType: string;
+  isSelected: boolean;
 };
