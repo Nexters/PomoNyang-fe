b5617d11c62e7c433bde64ca3718d64c7b1407f8
young-do <yd199323@gmail.com>
2025-03-26 00:41:40 +0900

    no가 없이 넘어오는 경우 대응

--- src/renderer/features/category/hooks/use-category.ts
+++ src/renderer/features/category/hooks/use-category.ts
@@ -4,13 +4,13 @@ import { Category } from '@/entities/category';
 import { QUERY_KEY } from '@/shared/constants';
 import { useAuthClient } from '@/shared/hooks';
 
-export const useCategory = (no: number) => {
+export const useCategory = (no?: number) => {
   const authClient = useAuthClient();
   return useQuery({
     queryKey: [QUERY_KEY.CATEGORIES, no],
     queryFn: async () => {
       return await authClient?.get<Category>(`/api/v1/categories/${no}`);
     },
-    enabled: !!authClient,
+    enabled: !!authClient && no != null,
   });
 };
