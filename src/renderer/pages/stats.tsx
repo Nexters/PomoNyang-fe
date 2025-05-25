9b798e5660d144c28547bc13900afa932b3eed29
young-do <yd199323@gmail.com>
2025-05-25 22:44:46 +0900

    각 영역만 일단 설정

--- src/renderer/pages/stats.tsx
+++ src/renderer/pages/stats.tsx
@@ -8,8 +8,37 @@ const StatsPage = () => {
 
   return (
     <SidebarLayout title={<StatsTitle date={date} onDateChange={setDate} />}>
-      <h1>Stats Page</h1>
-      <p>This is the stats page.</p>
+      <div className="flex h-full flex-col gap-5 overflow-y-auto pt-3">
+        <div>
+          <h2 className="header-4 px-4 py-5">총 집중시간</h2>
+          <div className="px-4 pb-5">
+            <div className="h-[100px] w-full bg-slate-400">3시간 37분 집중했어요!</div>
+          </div>
+        </div>
+        <div>
+          <h2 className="header-4 px-4 py-5">
+            집중 기록 <span className="text-text-accent-1">5</span>
+          </h2>
+          <div className="px-4 pb-5">
+            <div className="h-[450px] w-full bg-slate-400">기록 영역</div>
+          </div>
+        </div>
+        <div>
+          <h2 className="header-4 px-4 py-5">집중 추세</h2>
+          <div className="px-4 pb-5">
+            <div className="h-[300px] w-full bg-slate-400">그래프 영역</div>
+          </div>
+        </div>
+        <div>
+          <h2 className="header-4 flex items-center gap-2 px-4 py-5">
+            <span>카테고리 랭킹</span>
+            <span className="subBody-r text-text-tertiary">5월 8일 - 5월 14일</span>
+          </h2>
+          <div className="px-4 pb-5">
+            <div className="h-[150px] w-full bg-slate-400">랭킹 영역</div>
+          </div>
+        </div>
+      </div>
     </SidebarLayout>
   );
 };
